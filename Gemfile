9ab6f5d72f3b9e3911d0f8df49353fa951c9b7a0
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-07-18 12:37:28 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'mysql'
+
+gem "haml"
+gem "compass"
+
+gem "devise"
+
+# Test environment
+group :test do
+  gem 'rspec-rails', "~>2.1.0"
+  gem 'cucumber-rails'
+  gem 'gherkin'
+  gem 'webrat' 
+  gem 'autotest'
+  gem 'autotest-rails'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GestionDesempeno::Application.config.secret_token = '4771ac4d05c4127f7e19c3d867dd4f9cf4f035c7a265b9b6a940554cab600ccf2abeb9fd6e0d7c7b2a414ae6e8f4fb0c862eb5851c428bd8a4c6302c1d5d3f09'
