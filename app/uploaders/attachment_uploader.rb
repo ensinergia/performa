e2dd56f03641c0aeabd47531401205fead37dae2
Alejandro Paz <vidriloco@gmail.com>
2011-08-25 10:02:15 -0500

    Adjustment to carrierwave uploader for tmp files store on heroku

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -17,6 +17,12 @@ class AttachmentUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  # Solution for heroku tmp directory restriction
+  # http://codingfrontier.com/carrierwave-on-heroku
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
