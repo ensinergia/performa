ae2729c58c8d2b258f792c8fc13119881094f724
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-01-12 17:03:26 -0600

    remove test migration

--- db/migrate/20120112225913_add_area_id_to_operating_cycle2.rb
+++
@@ -1,9 +0,0 @@
-class AddAreaIdToOperatingCycle2 < ActiveRecord::Migration
-  def self.up
-    add_column :operating_cycles, :area_id, :integer
-  end
-
-  def self.down
-    remove_column :operating_cycles, :area_id
-  end
-end
