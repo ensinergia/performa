7a5f322288180866f7919f244f9316ea4ac33d66
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-01-23 16:03:26 -0600

    delete operative Cycles STEPS

--- public/javascripts/operating_edit.js
+++ public/javascripts/operating_edit.js
@@ -16,36 +16,52 @@ $(document).ready(function(){
 		add_input('services');
 		return false;
 	});
-	
+
 
 	$(".modify").click(function(){
 		id=$(this).attr('rel');
 		$("#"+id).addClass('editable');
 		return false;
 	});
-	
+
 	$(".delete").click(function(){
 		div=$(this).attr('rel');
-		alert(div);
 		if(div[4]=='new'){
 			$("#"+div).remove();
 		}else{
 			st=div.split('_');
 			id=st[6];
 			type=st[2];
-			
+
 			des="<input type='text' name='operating_cycle["+type+"_attributes]["+id+"][_destroy]' id='operating_cycle_"+type+"_attributes_new_"+id+"_destroy' class='hidden' value='1'>"
-		$("#"+div).append(des);
-		$("#"+div).addClass('hidden');
+			$("#"+div).append(des);
+			$("#"+div).addClass('hidden');
 		}
 		return false;
 	});
 
 
+
+	$(".add_step").click(function(){
+		id=$(this).attr('rel');
+		add_steps_input(id);
+		return false;
+	});
 	
-		$(".add_step").click(function(){
-			id=$(this).attr('rel');
-			add_steps_input(id);
+	
+		$(".delete_step").click(function(){
+			div=$(this).attr('rel');
+			st=div.split('_');
+			if(st[4]=='new'){
+				$("#"+div).remove();
+			}else{
+				
+				id=st[4];
+				id2=st[6];
+				des="<input type='text' name='operating_cycle[stages_attributes]["+id+"][steps_attributes]["+id2+"][_destroy]' id='operating_cycle_"+id+"_attributes_new_"+id2+"_destroy' class='hidden' value='1'>"
+				$("#"+div).append(des);
+				$("#"+div).addClass('hidden');
+			}
 			return false;
 		});
 
@@ -54,54 +70,55 @@ $(document).ready(function(){
 
 
 
+function add_input(type){
 
-  function add_input(type){
-	
-		value=$("#"+type+"_input").val();
-		if(value!=""){
-			id=Math.floor(Math.random()*1000);
-			inputs=$("#"+type+" .no_edit_input").length;
-			if(inputs%2==0)
-			classs='light-gray';
-			else
-			classs='gray';
-			
-			
-			inp='';
-			if (type=='stages'){
-				steps='<div id="steps_'+id+'" class="hidden"></div>';
-				inp='<div class="clear"></div><div class="steps_container"><label><strong> Pasos </strong></label><br><br><div class="align-left left" style="width:100%"><input id="steps_'+id+'_input" style="width:80%"><a id="add_step_'+id+'" rel="'+id+'" href="" class="add_step">+ Agregar Paso</a></div><div class="clear"></div><br/>'+steps+'</div>';
+	value=$("#"+type+"_input").val();
+	if(value!=""){
+		id=Math.floor(Math.random()*1000);
+		inputs=$("#"+type+" .no_edit_input").length;
+		if(inputs%2==0)
+		classs='light-gray';
+		else
+		classs='gray';
 
-			}
 
-			input="<div class='"+classs+"' id='operating_cycle_"+type+"_attributes_new_"+id+"_div'> <div class='left'><input type='text' size='50' name='operating_cycle["+type+"_attributes]["+id+"][name]' id='operating_cycle_"+type+"_attributes_new_"+id+"_name' class='no_edit_input' value='"+value+"'></div>"
-			links='<div class="right"><a class="modify" href="" rel="operating_cycle_'+type+'_attributes_new_'+id+'_name"><img src="/images/editar_ico_up.png?" class="button_to_edit" alt="Editar_ico_up"></a><a rel="operating_cycle_'+type+'_attributes_new_'+id+'_div" data-method="delete" class="delete" href=""><img src="/images/borrar_ico_up.png" class="button_to_delete" alt="Borrar_ico_up"></a></div>';
-			clear='<div class="clear"></div></div>';
-			$("#"+type).append(input+links+inp+clear);
-			$("#"+type).removeClass("hidden");
-			$("#"+type+"_input").val("");
-			
-			$(".modify").click(function(){
-				id=$(this).attr('rel');
-				$("#"+id).addClass('editable');
-				return false;
-			});
-			
-			$(".delete").click(function(){
-				id=$(this).attr('rel');
-				$("#"+id).remove();
-				return false;
-			});
-		
-		
-			$(".add_step").click(function(){
-				id=$(this).attr('rel');
-				add_steps_input(id);
-				return false;
-			});
-		
-		}	
-	
+		inp='';
+		class_delete='delete '
+		if (type=='stages'){
+			steps='<div id="steps_'+id+'" class="hidden"></div>';
+			inp='<div class="clear"></div><div class="steps_container"><label><strong> Pasos </strong></label><br><br><div class="align-left left" style="width:100%"><input id="steps_'+id+'_input" style="width:80%"><a id="add_step_'+id+'" rel="'+id+'" href="" class="add_step">+ Agregar Paso</a></div><div class="clear"></div><br/>'+steps+'</div>';
+			class_delete='delete_step '
+
+		}
+
+		input="<div class='"+classs+"' id='operating_cycle_"+type+"_attributes_new_"+id+"_div'> <div class='left'><input type='text' size='50' name='operating_cycle["+type+"_attributes]["+id+"][name]' id='operating_cycle_"+type+"_attributes_new_"+id+"_name' class='no_edit_input' value='"+value+"'></div>"
+		links='<div class="right"><a class="modify" href="" rel="operating_cycle_'+type+'_attributes_new_'+id+'_name"><img src="/images/editar_ico_up.png?" class="button_to_edit" alt="Editar_ico_up"></a><a rel="operating_cycle_'+type+'_attributes_new_'+id+'_div" data-method="delete" class="'+class_delete+'" href=""><img src="/images/borrar_ico_up.png" class="button_to_delete" alt="Borrar_ico_up"></a></div>';
+		clear='<div class="clear"></div></div>';
+		$("#"+type).append(input+links+inp+clear);
+		$("#"+type).removeClass("hidden");
+		$("#"+type+"_input").val("");
+
+		$(".modify").click(function(){
+			id=$(this).attr('rel');
+			$("#"+id).addClass('editable');
+			return false;
+		});
+
+		$(".delete").click(function(){
+			id=$(this).attr('rel');
+			$("#"+id).remove();
+			return false;
+		});
+
+
+		$(".add_step").click(function(){
+			id=$(this).attr('rel');
+			add_steps_input(id);
+			return false;
+		});
+
+	}	
+
 }
 
 
@@ -122,7 +139,7 @@ function add_steps_input(id){
 		$("#steps_"+id).removeClass("hidden");
 		$("#steps_"+id+"_input").val("");
 
-			
+
 
 		$(".modify_step").click(function(){
 			id=$(this).attr('rel');
@@ -131,10 +148,21 @@ function add_steps_input(id){
 		});
 
 		$(".delete_step").click(function(){
-			id=$(this).attr('rel');
-			$("#"+id).remove();
+			div=$(this).attr('rel');
+			st=div.split('_');
+			if(st[4]=='new'){
+				$("#"+div).remove();
+			}else{
+				
+				id=st[4];
+				id2=st[6];
+				des="<input type='text' name='operating_cycle[stages_attributes]["+id+"][steps_attributes]["+id2+"][_destroy]' id='operating_cycle_"+id+"_attributes_new_"+id2+"_destroy' class='hidden' value='1'>"
+				$("#"+div).append(des);
+				$("#"+div).addClass('hidden');
+			}
 			return false;
 		});
+
 
 	}
 
