9b150b48aa423976a631fd051904df43b1022e8c
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-01-25 13:27:07 -0600

    Create Projects model

---
+++ db/migrate/20120123231134_create_projects.rb
@@ -0,0 +1,24 @@
+class CreateProjects < ActiveRecord::Migration
+  def self.up
+    create_table  :projects do |t|
+      t.integer   :area_id
+      t.indeger   :user_id
+      t.integer   :company_id
+      t.string    :name
+      t.text      :description
+      t.string    :type
+      t.text      :reason
+      t.integer   :leader_id
+      t.string    :length
+      t.datetime  :init_date
+      t.datetime  :final_date
+      t.text      :product
+      t.string    :investment
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :projects
+  end
+end
