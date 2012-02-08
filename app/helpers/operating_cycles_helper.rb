990afc129f78265f530685fc54d3d0f2803fed5f
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-02-08 13:04:48 -0600

    Fix next steps null

--- app/helpers/operating_cycles_helper.rb
+++ app/helpers/operating_cycles_helper.rb
@@ -6,6 +6,7 @@ module OperatingCyclesHelper
     stages=operating_cycle.stages	
     clients=operating_cycle.clients	
     products=operating_cycle.services	
+
     unless stages.empty? || clients.empty? || products.empty?
 
       clients=get_clients(operating_cycle)
@@ -28,59 +29,62 @@ module OperatingCyclesHelper
       ic=0;
       stages.each_with_index do |stage,index|
         steps_arr=stage.steps
-        labels=""
-        steps=steps_arr.enum_for(:each_with_index).collect do |x, i| 
-          stname=wrap_word(x.name,3)
-          height=(x.name.split(" ").count / 3) 
-          if height==0
-             height=0.3
-          else
-             height=height*0.2
-          end      
-          
-          if height<0.3
-            height=0.3
+        unless steps_arr.empty?
+          labels=""
+          steps=steps_arr.enum_for(:each_with_index).collect do |x, i| 
+            stname=wrap_word(x.name,3)
+            height=(x.name.split(" ").count / 3) 
+            if height==0
+              height=0.3
+            else
+              height=height*0.2
+            end      
+
+            if height<0.3
+              height=0.3
+            end
+            labels+='"'+x.id.to_s+'.-'+stage.id.to_s+'" [label="'+stname+'", shape= box, style=filled,color=white,fontsize=9,fixedsize=true, width=1.8,height='+height.to_s+',text-wrap= auto, margin=0];'	
+
+            if index==0 && i==0
+              from_clients+='"clients" -> "'+x.id.to_s+'.-'+stage.id.to_s+'"[style=dashed];'
+            end
+
+            if i==steps_arr.count-1
+              to_products+='"'+x.id.to_s+'.-'+stage.id.to_s+'"'+" -> products";	
+              to_products+=	index==stages.count-1 ? "[style=dashed] ; " : "[style=invis] ; " 
+            end
+
+            if i==0
+              from_clients+=index==0 ? " " : '"clients" -> '+'"'+x.id.to_s+'.-'+stage.id.to_s+'"'+"[style=invis]; ";
+              '"'+x.id.to_s+'.-'+stage.id.to_s+'"'	
+            else
+              nextt='"'+x.id.to_s+'.-'+stage.id.to_s+'"'
+              str= i==steps_arr.count-1 ?	'-> "'+x.id.to_s+'.-'+stage.id.to_s+'" ; ' : '-> "'+x.id.to_s+'.-'+stage.id.to_s+'" ; ' + nextt	
+
+            end	
+
           end
-          labels+='"'+x.id.to_s+'.-'+stage.id.to_s+'" [label="'+stname+'", shape= box, style=filled,color=white,fontsize=9,fixedsize=true, width=1.8,height='+height.to_s+',text-wrap= auto, margin=0];'	
 
-          if index==0 && i==0
-            from_clients+='"clients" -> "'+x.id.to_s+'.-'+stage.id.to_s+'"[style=dashed];'
-          end
-
-          if i==steps_arr.count-1
-            to_products+='"'+x.id.to_s+'.-'+stage.id.to_s+'"'+" -> products";	
-            to_products+=	index==stages.count-1 ? "[style=dashed] ; " : "[style=invis] ; " 
-          end
-
-          if i==0
-            from_clients+=index==0 ? " " : '"clients" -> '+'"'+x.id.to_s+'.-'+stage.id.to_s+'"'+"[style=invis]; ";
-            '"'+x.id.to_s+'.-'+stage.id.to_s+'"'	
-          else
-            nextt='"'+x.id.to_s+'.-'+stage.id.to_s+'"'
-            str= i==steps_arr.count-1 ?	'-> "'+x.id.to_s+'.-'+stage.id.to_s+'" ; ' : '-> "'+x.id.to_s+'.-'+stage.id.to_s+'" ; ' + nextt	
-
+          if stages.count-1>index
+            nexts=stages[index+1].steps.first
+            x=stages[index].steps.last
+            unless nexts.nil?
+              follow+='"'+x.id.to_s+'.-'+stage.id.to_s+'" -> "'+nexts.id.to_s+'.-'+stages[index+1].id.to_s+'"[style=dashed] ;'
+            end
           end	
 
-        end
+          ic= index>6 ?   0 : ic
+          stages_str+='subgraph cluster_'+stage.id.to_s+' {
+          fontsize=9,
+          style=filled;
+          color="'+colors[ic]+'";
+          '+labels+'
+          '+steps.join+'
+          label = "'+stage.name+'";
+          }'		
 
-        if stages.count-1>index
-          nexts=stages[index+1].steps.first
-          x=stages[index].steps.last
-          follow+='"'+x.id.to_s+'.-'+stage.id.to_s+'" -> "'+nexts.id.to_s+'.-'+stages[index+1].id.to_s+'"[style=dashed] ;'
-        end	
-
-        ic= index>6 ?   0 : ic
-        stages_str+='subgraph cluster_'+stage.id.to_s+' {
-        fontsize=9,
-        style=filled;
-        color="'+colors[ic]+'";
-        '+labels+'
-        '+steps.join+'
-        label = "'+stage.name+'";
-        }'		
-
-        ic+=1
-
+          ic+=1
+        end   
       end	
 
       code='digraph G {
@@ -132,18 +136,18 @@ module OperatingCyclesHelper
     products
   end  
 
-  
+
   def wrap_word(name,nw)
     arrname=name.split(" ");
     fname=""
     tab='\n'
-        arrname.each_with_index do |name,inc|
-          fname+=" "+name
-          if inc%nw==0 and inc!=0
-            fname+=tab
-          end  
-        end  
-        fname
+    arrname.each_with_index do |name,inc|
+      fname+=" "+name
+      if inc%nw==0 and inc!=0
+        fname+=tab
+      end  
+    end  
+    fname
   end  
 
 
