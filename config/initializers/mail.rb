03959c4d4002cf163f9738ce499a6b58d21cbb88
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-02-22 12:39:24 -0600

    Sendgrid setup

---
+++ config/initializers/mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => "app695661@heroku.com",
+  :password       => "m8eetf55",
+  :domain         => 'heroku.com'
+}
+ActionMailer::Base.delivery_method = :smtp
