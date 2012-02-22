7f9360c2286942654679ec0a381bb4d0283ce94c
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-02-22 12:49:27 -0600

    Set default url options for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,11 @@ GestionDesempeno::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
+
+  config.action_mailer.default_url_options = { :host => 'ensinergia.net' }
+
+
+
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
