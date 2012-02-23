e51d0918c4fb74c71b702863c9162c76ec14a8fe
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-02-22 19:51:59 -0600

    Create new person with confirmation devise

---
+++ db/migrate/20120223012522_add_confirmable_to_user.rb
@@ -0,0 +1,12 @@
+class AddConfirmableToUser < ActiveRecord::Migration
+  def self.up
+    change_table(:users) do |t|
+      t.confirmable
+    end
+    add_index :users, :confirmation_token,   :unique => true
+
+  end
+
+  def self.down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120215021618) do
+ActiveRecord::Schema.define(:version => 20120223012522) do
 
   create_table "analyses", :force => true do |t|
     t.string   "content"
@@ -314,8 +314,12 @@ ActiveRecord::Schema.define(:version => 20120215021618) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
