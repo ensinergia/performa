56a49d453b01696dc843200942666627b57a2cb7
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-02-24 11:46:42 -0600

    Confirm account manually

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,16 @@ class HomeController < ActionController::Base
   def index
   end
   
-  def confirma
+  def confirm
+    user=User.where(:id=>params[:id], :confirmation_token=>params[:confirmation_token]).first
+    unless user.nil?
+      user.confirmed_at=Time.now
+      user.save
+      redirect_to new_user_session_url, :notice => t('views.mailer.thanks.confirm')
+    else
+      redirect_to new_user_session_url, :notice=>t('views.mailer.confirm.bad')
+    end    
+    
   end  
   
 end
