2679785050d67649aa8044ef64c426df02048993
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-02-24 21:18:19 -0600

    New  confirm data form  for invites users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,25 @@
+#encoding: utf-8
+require 'subdomain_guards'
 class HomeController < ActionController::Base
-  layout 'application'
+  include SubdomainGuards
+  layout 'application_index_page'
+
+  
   
   def index
   end
   
   def confirm
-    user=User.where(:id=>params[:id], :confirmation_token=>params[:confirmation_token]).first
-    unless user.nil?
-      user.confirmed_at=Time.now
-      user.save
-      redirect_to new_user_session_url, :notice => t('views.mailer.thanks.confirm')
-    else
-      redirect_to new_user_session_url, :notice=>t('views.mailer.confirm.bad')
+    @user=User.where(:id=>params[:id], :confirmation_token=>params[:confirmation_token]).first
+    unless @user.nil?
+      @user.confirmed_at=Time.now
+      @user.save
+      unless @user.password.nil?
+        redirect_to new_user_session_url, :notice => t('views.mailer.thanks.confirm')
+      end 
+      sign_in @user 
+    else      
+        redirect_to new_user_session_url, :notice=>t('views.mailer.confirm.bad')
     end    
     
   end  
