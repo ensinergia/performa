65eacc8651b0bf51ca9cb3ee137fdaf76c3ba94e
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-03-27 16:51:59 -0600

    dynamic pointer graph

--- public/javascripts/pointers.js
+++ public/javascripts/pointers.js
@@ -82,19 +82,9 @@ $(document).ready(function() {
 		$(" .no_edit_input").removeAttr("disabled");
 	});
 
+});
 
 
-
-}
-
-);
-
-
-//Google charts
-google.load('visualization', '1', {packages: ['corechart']});
-
-
-google.setOnLoadCallback(drawVisualization);
 
 
 function updateGrid(){
@@ -118,25 +108,11 @@ function updateGrid(){
 
 function drawVisualization() {
 	// Some raw data (not necessarily accurate)
-	var data = google.visualization.arrayToDataTable([
-		['Mes', 'Resultado del Periodo','Meta del Periodo'],
-		['Ene', 800, 938],
-		['Feb', 1000, 1120],
-		['Mar', 1200, 1167],
-		['Abr', 990, 1110],
-		['May', 800, 691],
-		['Jun', 1000, 1510],
-		['Jul', 1500, 1410],
-		['Ago', 1600, 1810],
-		['Sep', 1800, 2010],
-		['Oct', 1700, 1910],
-		['Nov', 1550, 1510],
-		['Dic', 1025, 1310],
-		]);
+	var data = google.visualization.arrayToDataTable(datagrid);
 
 		var options = {
 			titlePosition: 'in',
-			title : 'Simulacion de Indicadores',
+			title : 'Simulación de Indicadores',
 			vAxis: {title: "Valor"},
 			hAxis: {title: "Meses"},
 			seriesType: "bars",
