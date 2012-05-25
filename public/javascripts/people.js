0ff0cd6a9d9aa9f8b24df99f09e76d90fc45acbd
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-05-25 17:20:06 -0500

    Add validations  for  people and areas

--- public/javascripts/people.js
+++ public/javascripts/people.js
@@ -6,17 +6,43 @@ $(document).ready(function(){
 		$("#area-form-"+id).html("");
 		$("#area-form-"+id).html(form);
 		$("#user_area_id").val(id);
-		
+
 		$(".cancel_person").click(function(){
 			$(".area-form").html("");
 			return false;
 		});
-		
-		
+
+
+
+
+
+
+		$(".validation").validator({
+			effect: 'wall',
+			container: '#errors',
+			lang: 'es',
+			// do not validate inputs when they are edited
+			errorInputEvent: null
+
+			// custom form submission logic
+		}).submit(function(e)  {
+
+			// when data is valid
+			if (!e.isDefaultPrevented()) {
+
+				// tell user that everything is OK
+				$("#errors").html("<h2>Correcto!</h2>");
+
+				// prevent the form data being submitted to the server
+				//e.preventDefault();
+			}
+
+		});
+
 		return false;
-		
-		
-		
+
+
+
 	});
 }
 );
