f9f21fba4ef1d2e817028004b4a8a46b01d733ba
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-06-14 18:24:58 -0500

    Change Second level menu

--- app/controllers/panoramas_controller.rb
+++ app/controllers/panoramas_controller.rb
@@ -6,7 +6,15 @@ class PanoramasController < ApplicationController
   before_filter :verify_subdomain
   
   def index
-    session['area_id']=='' || session[:area_id].to_s==session[:main_area_id].to_s  ? render( :layout => 'application_index_page') : render('index_area',:layout => 'application_index_page')
+    if session['area_id']=='' || session[:area_id].to_s==session[:main_area_id].to_s  
+          render( :layout => 'application_index_page') 
+    else      
+       if @selected_area.alevel==1
+         render('index_inter',:layout => 'application_index_page')
+        else
+          render('index_area',:layout => 'application_index_page')
+        end   
+   end
   end
   
 end
