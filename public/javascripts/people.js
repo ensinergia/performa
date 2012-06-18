6968084a01c610f304da7ed0795c02114bd2c4b9
Sergio Aguirre <sergio.aguirre@live.com.mx>
2012-06-18 15:46:26 -0500

    fix new user issue at area section

--- public/javascripts/people.js
+++ public/javascripts/people.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 	$(".new_person").click(function(){
 		$(".area-form").html("");
 		id=$(this).attr('rel');
+		$("#user_area_id").val(id);
 		form=$('#new-person').html();
 		$("#area-form-"+id).html("");
 		$("#area-form-"+id).html(form);
-		$("#user_area_id").val(id);
+		
 
 		$(".cancel_person").click(function(){
 			$(".area-form").html("");
